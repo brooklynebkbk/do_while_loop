a8f8b6225df4cd11201fe02c00cd1ab75eba2fe7
Brooklyne Finni <brooklyne.finni@gmail.com>
2019-07-27 16:19:50 -0400

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+/*
+//exits out of program when good bye is typed
+int main()
+{
+  string str;
+  do {
+    cout << "Enter text: ";
+    getline (cin, str);
+    cout << "You entered: " << str << '\n';
+  } while (str != "goodbye");
+
+  return 0;
+
+}
+*/
+
+
+/*
+//ineligible
+int main()
+{
+  ineligible:
+  cout << "Enter your age: ";
+  int age;
+  cin >> age;
+  if (age < 18){
+    cout << "You are not eligible to vote.\n";
+    goto ineligible;
+  }
+  else{
+    cout << "You are eligible to vote!";
+  }
+
+  return 0;
+}*/
+
+
